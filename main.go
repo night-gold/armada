64cc8165d63fed84aba2f20af005f24d2eca7e1a
Braley <braley.jo@gmail.com>
2019-07-30 11:52:35 +0200

    fixing struct default value condition mistake

--- main.go
+++ main.go
@@ -1,72 +1,73 @@
 package main
 
 import (
-    "flag"
-    git "gopkg.in/src-d/go-git.v4"
-    "gopkg.in/yaml.v2"
-    "io/ioutil"
-    "os"
-    "os/exec"
-    "gopkg.in/src-d/go-git.v4/plumbing"
-    "github.com/matthewrsj/copy"
-    "github.com/night-gold/armada/utils"
+	"flag"
+	"io/ioutil"
+	"os"
+	"os/exec"
+
+	"github.com/matthewrsj/copy"
+	"github.com/night-gold/armada/utils"
+	git "gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing"
+	"gopkg.in/yaml.v2"
 )
 
 func main() {
-    var config Config
+	var config Config
 
-    file := flag.String("f", "armada.yaml", "Armada package file to load")
-    help := flag.Bool("h", false, "Print the help")
-    flag.Parse()
-    
-    if *help {
+	file := flag.String("f", "armada.yaml", "Armada package file to load")
+	help := flag.Bool("h", false, "Print the help")
+	flag.Parse()
+
+	if *help {
 		flag.PrintDefaults()
 	} else {
-        source, err := ioutil.ReadFile(*file)
-        if err != nil {
-            panic(err)
-        }
-        err = yaml.Unmarshal(source, &config)
-        if err != nil {
-            panic(err)
-        }
+		source, err := ioutil.ReadFile(*file)
+		if err != nil {
+			panic(err)
+		}
+		err = yaml.Unmarshal(source, &config)
+		if err != nil {
+			panic(err)
+		}
 
-        for _, repo := range config.Repo {
-            if repo.Version == ""{
-                repo.Version = "master"
-            }
-            if repo.Git == ""{
-                repo.Git = "https://github.com"
-            }
-            if repo.User == ""{
-                repo.User = "armada"
-            }
-            if repo.Folder != ""{
-                repo.Folder = repo.Repository
-            }
-            if repo.Overlays != ""{
-                repo.Overlays = "apply"
-            }
-            _, err := git.PlainClone(os.TempDir()+"/"+ repo.Repository, false, &git.CloneOptions{
-                URL: repo.Git + "/" + repo.User + "/" + repo.Repository,
-                Progress: os.Stdout,
-                ReferenceName: plumbing.ReferenceName("refs/heads/" + repo.Version),
-                SingleBranch:  true,
-            })
-            if err != nil {
-                panic(err)
-            }
+		for _, repo := range config.Repo {
+			if repo.Version == "" {
+				repo.Version = "master"
+			}
+			if repo.Git == "" {
+				repo.Git = "https://github.com"
+			}
+			if repo.User == "" {
+				repo.User = "armada"
+			}
+			if repo.Folder == "" {
+				repo.Folder = repo.Repository
+			}
+			if repo.Overlays == "" {
+				repo.Overlays = "apply"
+			}
+			_, err := git.PlainClone(os.TempDir()+"/"+repo.Repository, false, &git.CloneOptions{
+				URL:           repo.Git + "/" + repo.User + "/" + repo.Repository,
+				Progress:      os.Stdout,
+				ReferenceName: plumbing.ReferenceName("refs/heads/" + repo.Version),
+				SingleBranch:  true,
+			})
+			if err != nil {
+				panic(err)
+			}
 
-            erro := copy.All(os.TempDir()+"/"+repo.Repository +"/base", repo.Folder + "/base")
-            if erro != nil {
-                panic(erro)
-            }
+			erro := copy.All(os.TempDir()+"/"+repo.Repository+"/base", repo.Folder+"/base")
+			if erro != nil {
+				panic(erro)
+			}
 
-            cmd := exec.Command("kubectl","kustomize", "overlays/"+ repo.Overlays)
-            utils.CmdOutputToFile(cmd, repo.Repository + ".yaml")
+			cmd := exec.Command("kubectl", "kustomize", "overlays/"+repo.Overlays)
+			utils.CmdOutputToFile(cmd, repo.Repository+".yaml")
 
-            os.RemoveAll(os.TempDir()+"/"+repo.Repository)
-            os.RemoveAll(repo.Folder)
-        }
-    }
+			os.RemoveAll(os.TempDir() + "/" + repo.Repository)
+			os.RemoveAll(repo.Folder + "/base")
+		}
+	}
 }
