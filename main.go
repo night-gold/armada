d5f47a4859152fd93a4d23d229bc08b3fcc011bf
night-gold <braley.jo@gmail.com>
2019-08-16 22:28:53 +0200

    removed unnecessary -h flag, fixing log usage

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"os"
 	"os/exec"
+	"log"
 
 	"github.com/matthewrsj/copy"
 	"github.com/night-gold/armada/utils"
@@ -17,57 +18,52 @@ func main() {
 	var config Config
 
 	file := flag.String("f", "armada.yaml", "Armada package file to load")
-	help := flag.Bool("h", false, "Print the help")
 	flag.Parse()
 
-	if *help {
-		flag.PrintDefaults()
-	} else {
-		source, err := ioutil.ReadFile(*file)
+	source, err := ioutil.ReadFile(*file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = yaml.Unmarshal(source, &config)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for _, repo := range config.Repo {
+		if repo.Version == "" {
+			repo.Version = "master"
+		}
+		if repo.Git == "" {
+			repo.Git = "https://github.com"
+		}
+		if repo.User == "" {
+			repo.User = "armada"
+		}
+		if repo.Folder == "" {
+			repo.Folder = repo.Repository
+		}
+		if repo.Overlays == "" {
+			repo.Overlays = "apply"
+		}
+		_, err := git.PlainClone(os.TempDir()+"/"+repo.Repository, false, &git.CloneOptions{
+			URL:           repo.Git + "/" + repo.User + "/" + repo.Repository,
+			Progress:      os.Stdout,
+			ReferenceName: plumbing.ReferenceName("refs/heads/" + repo.Version),
+			SingleBranch:  true,
+		})
 		if err != nil {
-			panic(err)
-		}
-		err = yaml.Unmarshal(source, &config)
-		if err != nil {
-			panic(err)
+			log.Panic(err)
 		}
 
-		for _, repo := range config.Repo {
-			if repo.Version == "" {
-				repo.Version = "master"
-			}
-			if repo.Git == "" {
-				repo.Git = "https://github.com"
-			}
-			if repo.User == "" {
-				repo.User = "armada"
-			}
-			if repo.Folder == "" {
-				repo.Folder = repo.Repository
-			}
-			if repo.Overlays == "" {
-				repo.Overlays = "apply"
-			}
-			_, err := git.PlainClone(os.TempDir()+"/"+repo.Repository, false, &git.CloneOptions{
-				URL:           repo.Git + "/" + repo.User + "/" + repo.Repository,
-				Progress:      os.Stdout,
-				ReferenceName: plumbing.ReferenceName("refs/heads/" + repo.Version),
-				SingleBranch:  true,
-			})
-			if err != nil {
-				panic(err)
-			}
-
-			erro := copy.All(os.TempDir()+"/"+repo.Repository+"/base", repo.Folder+"/base")
-			if erro != nil {
-				panic(erro)
-			}
-
-			cmd := exec.Command("kubectl", "kustomize", "overlays/"+repo.Overlays)
-			utils.CmdOutputToFile(cmd, repo.Repository+".yaml")
-
-			os.RemoveAll(os.TempDir() + "/" + repo.Repository)
-			os.RemoveAll(repo.Folder + "/base")
+		erro := copy.All(os.TempDir()+"/"+repo.Repository+"/base", repo.Folder+"/base")
+		if erro != nil {
+			log.Panic(erro)
 		}
+
+		cmd := exec.Command("kubectl", "kustomize", "overlays/"+repo.Overlays)
+		utils.CmdOutputToFile(cmd, repo.Repository+".yaml")
+
+		os.RemoveAll(os.TempDir() + "/" + repo.Repository)
+		os.RemoveAll(repo.Folder + "/base")
 	}
 }
