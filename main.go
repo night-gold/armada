dffbeb53644415972ecb52ffe4329e6031679320
Braley <braley.jo@gmail.com>
2019-08-26 09:51:03 +0200

    removed printed test, added cleanfolder function in panic

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -77,8 +76,8 @@ func main() {
 			//output, err := cmd.CombinedOutput()
 			err := cmd.Run()
 			if err != nil {
+				cleanFolder(repo.Repository, repo.Folder)
 				log.Panic(err)
-				fmt.Println("tets")
 			}
 		} else {
 			cmd := exec.Command("kubectl", "kustomize", "overlays/"+repo.Overlays)
