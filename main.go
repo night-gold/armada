307e5159f32f7e89043607a11bc84ad41b799b07
Braley <braley.jo@gmail.com>
2019-08-26 13:44:55 +0200

    testing file existence, issue with return error

--- main.go
+++ main.go
@@ -25,6 +25,10 @@ func main() {
 	apply := flag.Bool("a", false, "Auto apply of the kustomize configuration")
 	flag.Parse()
 
+	a, err := utils.FileExists(*file)
+	if a == false (
+		fmt.Println(err)
+	)
 	source, err := ioutil.ReadFile(*file)
 	if err != nil {
 		log.Fatal(err)
