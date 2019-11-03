097f673a3ec51c9586774bc54ea7a4d4154a5637
night-gold <braley.jo@gmail.com>
2019-11-03 22:43:09 +0100

    Removed git calls, refactoring all calls

--- package.go
+++ package.go
@@ -3,24 +3,14 @@ package main
 // Package struct contains field refering to a specific package and one deployment
 type Package struct {
 	Name       string
-	Git        Git
 	Deployment Deployment
 }
 
 // Pack set functions for package struct
 type Pack interface {
-	setGit(git Git)
 	setDeployment(dep Deployment)
 }
 
-func (p *Package) setGit() {
-	p.Git.checkRepository()
-	p.Git.setGit()
-	p.Git.setVersion()
-	p.Git.setUser()
-}
-
 func (p *Package) setDeployment(overlays string) {
-	p.Deployment.setFolder(p.Git.Repository)
 	p.Deployment.setOverlays(overlays)
 }
